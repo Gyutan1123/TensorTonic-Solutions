91582f7574a70df1b86976612dbb207fee86d8b3
Gyutaaan <106474758+Gyutan1123@users.noreply.github.com>
2026-03-25 22:18:06 +0900

    Add solution for ddpm-forward

---
+++ ddpm/ddpm-forward/ddpm-forward.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def get_alpha_bar(betas: np.ndarray) -> np.ndarray:
+    """
+    Compute cumulative product of (1 - beta).
+    """
+    return np.cumprod(1-betas)
+
+def forward_diffusion(
+    x_0: np.ndarray,
+    t: int,
+    betas: np.ndarray
+) -> tuple:
+    """
+    Sample x_t from q(x_t | x_0).
+    """
+
+    alpha_bar_t = get_alpha_bar(betas)[t]
+    e = np.random.normal(size=x_0.shape)
+
+    x_t = np.sqrt(alpha_bar_t)*x_0 + (1-np.sqrt(alpha_bar_t))*e
+
+    return x_t, e
