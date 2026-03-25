68e94f6b39fec9704e6992600ef7211a1a7911bc
Gyutaaan <106474758+Gyutan1123@users.noreply.github.com>
2026-03-25 23:27:13 +0900

    Update solution for ddpm-sampling

--- ddpm/ddpm-sampling/ddpm-sampling.py
+++ ddpm/ddpm-sampling/ddpm-sampling.py
@@ -15,12 +15,12 @@ def reverse_step(
     """
     Perform one reverse diffusion step.
     """
-    alpha_t = (1-betas)[t]
-    alpha_bar_t = get_alpha_bar(betas)[t]
+    alpha_t = (1-betas)[t-1]
+    alpha_bar_t = get_alpha_bar(betas)[t-1]
     u = (x_t - (1 - alpha_t) * epsilon_pred / np.sqrt(1-alpha_bar_t)) / np.sqrt(alpha_t)
 
     if t > 1:
-        z = np.sqrt(betas[t]) * np.random.normal(size=x_t.shape)
+        z = np.sqrt(betas[t-1]) * np.random.normal(size=x_t.shape)
     else:
         z = 0
     return u + z
@@ -38,8 +38,8 @@ def ddpm_sample(
     """
     x_t = np.random.normal(size=shape)
 
-    for t in range(1, T+1, -1):
+    for t in range(T, 0, -1):
         eps_pred = model_predict(x_t, t)
-        x_t1 = reverse_step(x_t, t, eps_pred, betas)
+        x_t = reverse_step(x_t, t, eps_pred, betas)
 
     return x_t
